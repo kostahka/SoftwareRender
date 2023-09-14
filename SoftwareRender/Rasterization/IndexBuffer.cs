0f07214a84264bce7644f0592a6d6b59f14922a5
Max <maks_kosten@mail.ru>
2023-09-14 13:25:18 +0300

    ZBuffer and TriangleIndexBuffer. Start rasterezation logic.

---
+++ SoftwareRender/Rasterization/IndexBuffer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace SoftwareRender.Rasterization
+{
+    internal unsafe class IndexBuffer
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        int[] buffer;
+        public IndexBuffer(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            buffer = new int[Width * Height];
+        }
+
+        public int GetIndex(int x, int y)
+        {
+            return buffer[x + y * Width];
+        }
+
+        public void SetIndex(int x, int y, int index)
+        {
+            buffer[x + y * Width] = index;
+        }
+
+        public void ClearBuffer()
+        {
+            for (int i = 0; i < Width * Height; i++)
+            {
+                buffer[i] = 0;
+            }
+        }
+    }
+}
--- SoftwareRender/RenderConveyor/RenderConveyor.cs
+++ SoftwareRender/RenderConveyor/RenderConveyor.cs
@@ -21,12 +21,16 @@ namespace SoftwareRender.RenderConveyor
 
         IShaderProgram? program = null;
         RenderCanvas canvas;
+        IndexBuffer indexBuffer;
+        FloatBuffer zBuffer;
         public RenderConv(RenderCanvas canv) 
         {
             canvas = canv;
             int viewportX = canvas.PixelWidth;
             int viewportY = canvas.PixelHeight;
             viewport = CreateViewport(0, 0, viewportX, viewportY, 0, 1);
+            indexBuffer = new IndexBuffer(canvas.PixelWidth, canvas.PixelHeight);
+            zBuffer = new FloatBuffer(canvas.PixelWidth, canvas.PixelHeight);
         }
     
         public void SetShaderProgram(IShaderProgram shaderProgram)
@@ -39,6 +43,146 @@ namespace SoftwareRender.RenderConveyor
             return (v1.X > -1 && v1.X < 1 && v1.Y > -1 && v1.Z < 1)
                 || (v2.X > -1 && v2.X < 1 && v2.Y > -1 && v2.Z < 1)
                 || (v3.X > -1 && v3.X < 1 && v3.Y > -1 && v3.Z < 1);
+        }
+        private float InterpolateZ(float z1, float z2, float t)
+        {
+            return 1 / (1 / z1 * t + 1 / z2 * (1 - t));
+        }
+        private void DrawTriangleIndex(Vector4 uv1, Vector4 uv2, Vector4 uv3, int index)
+        {
+            Vector4 up;
+            Vector4 middle;
+            Vector4 down;
+
+            if (uv1.Y > uv2.Y)
+            {
+                if (uv1.Y > uv3.Y)
+                {
+                    up = uv1;
+                    if (uv2.Y > uv3.Y)
+                    {
+                        middle = uv2;
+                        down = uv3;
+                    }
+                    else
+                    {
+                        middle = uv3;
+                        down = uv2;
+                    }
+                }
+                else
+                {
+                    up = uv3;
+                    middle = uv1;
+                    down = uv2;
+                }
+            }
+            else
+            {
+                if (uv2.Y > uv3.Y)
+                {
+                    up = uv2;
+                    if (uv1.Y > uv3.Y)
+                    {
+                        middle = uv1;
+                        down = uv3;
+                    }
+                    else
+                    {
+                        middle = uv3;
+                        down = uv1;
+                    }
+                }
+                else
+                {
+                    up = uv3;
+                    middle = uv2;
+                    down = uv1;
+                }
+            }
+
+            for(int y = Math.Max((int)middle.Y, 0); y <= Math.Min((int)up.Y, indexBuffer.Height - 1); y++)
+            {
+                float deltaUp = y - up.Y;
+                float t1 = (down.Y - up.Y) / (deltaUp);
+                float t2 = (middle.Y - up.Y) / (deltaUp);
+                int x0 = (int)(up.X + (down.X - up.X) * t1);
+                int x1 = (int)(up.X + (middle.X - up.X) * t2);
+
+                float z0 = InterpolateZ(up.Z, down.Z, t1);
+                float z1 = InterpolateZ(up.Z, middle.Z, t2);
+
+                int startX;
+                int endX;
+                float startZ;
+                float endZ;
+                if (x0 > x1)
+                {
+                    startX = x1;
+                    endX = x0;
+                    startZ = z1;
+                    endZ = z0;
+                }
+                else
+                {
+                    startX = x0;
+                    endX = x1;
+                    startZ = z0;
+                    endZ = z1;
+                }
+
+                for (int x = Math.Max(startX, 0); x <= Math.Min(endX, indexBuffer.Width - 1); x++)
+                {
+                    float t = (float)(x - startX) / (endX - startX);
+                    float z = InterpolateZ(startZ, endZ, t);
+                    if(zBuffer.GetValue(x, y) > z)
+                    {
+                        indexBuffer.SetIndex(x, y, index);
+                        zBuffer.SetValue(x, y, z);
+                    }
+                }
+            }
+            for (int y = Math.Max((int)down.Y, 0); y < Math.Min((int)middle.Y, indexBuffer.Height); y++)
+            {
+                float deltaDown = y - down.Y;
+                float t1 = (up.Y - down.Y) / (deltaDown);
+                float t2 = (middle.Y - down.Y) / (deltaDown);
+                int x0 = (int)(down.X + (up.X - down.X) * t1);
+                int x1 = (int)(down.X + (middle.X - down.X) * t2);
+
+                float z0 = InterpolateZ(down.Z, up.Z, t1);
+                float z1 = InterpolateZ(down.Z, middle.Z, t2);
+
+                int startX;
+                int endX;
+                float startZ;
+                float endZ;
+                if (x0 > x1)
+                {
+                    startX = x1;
+                    endX = x0;
+                    startZ = z1;
+                    endZ = z0;
+                }
+                else
+                {
+                    startX = x0;
+                    endX = x1;
+                    startZ = z0;
+                    endZ = z1;
+                }
+
+                for (int x = Math.Max(startX, 0); x <= Math.Min(endX, indexBuffer.Width - 1); x++)
+                {
+                    float t = (float)(x - startX) / (endX - startX);
+                    float z = InterpolateZ(startZ, endZ, t);
+                    if (zBuffer.GetValue(x, y) > z)
+                    {
+                        indexBuffer.SetIndex(x, y, index);
+                        zBuffer.SetValue(x, y, z);
+                    }
+                }
+            }
         }
         public static Matrix4x4 CreateViewport(float x, float y, float width, float height, float minDepth, float maxDepth)
         {
@@ -128,6 +272,9 @@ namespace SoftwareRender.RenderConveyor
                     }
                 );
 
+                //indexBuffer.ClearBuffer();
+                //zBuffer.ClearBuffer(1);
+
                 int triangleCountPerThread = (indexesCount / 3) / 4;
                 Parallel.For(0, 4,
                     (int j) =>
@@ -152,9 +299,12 @@ namespace SoftwareRender.RenderConveyor
                                 canvas.DrawLineBresenhem((int)uv2.X, (int)uv2.Y, (int)uv3.X, (int)uv3.Y, new Vector3(0, 0, 0));
                                 canvas.DrawLineBresenhem((int)uv1.X, (int)uv1.Y, (int)uv3.X, (int)uv3.Y, new Vector3(0, 0, 0));
                                 */
+
                                 DrawDDALine(uv1.X, uv1.Y, uv2.X, uv2.Y, new Vector3(0, 0, 0));
                                 DrawDDALine(uv3.X, uv3.Y, uv2.X, uv2.Y, new Vector3(0, 0, 0));
                                 DrawDDALine(uv1.X, uv1.Y, uv3.X, uv3.Y, new Vector3(0, 0, 0));
+                                
+                                //DrawTriangleIndex(uv1, uv2, uv3, i + 1);
                                 // TODO: Fragment process
                             }
                         }
